d4f87e6bcddb973a3df1c9cffb36cb8696f48ce7
kristis295 <mazonas295@gmail.com>
2019-12-09 19:18:19 +0200

    L4 done

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System.Drawing;
+
+namespace SMA4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void PreparareForm(float xmin, float xmax, float ymin, float ymax)
+        {
+
+            float x_grids = 10;
+            //double xmin = 0; double xmax = 2 * Math.PI;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.MajorTickMark.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            float y_grids = 16;
+            //double ymin = -1; double ymax = 1;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.MajorTickMark.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax;
+            chart1.ChartAreas[0].AxisY.Minimum = ymin;
+            chart1.ChartAreas[0].AxisY.Maximum = ymax;
+
+            chart1.Legends[0].Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorX.Interval = 0.01;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorY.Interval = 0.01;
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1225, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eulerio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(38, 20);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1155, 584);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(38, 632);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1155, 200);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1225, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "IV RG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 860);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SMA4
+{
+    public partial class Form1 : Form
+    {
+        private const double M = 10;
+        private const double V0 = 100;
+        private const double H0 = 30;
+        private const double K1 = 0.05;
+        private const double K2 = 0.01;
+        private const double G = 9.8;
+        
+        private const double DELTA_T = 0.01;
+
+        Series Fx1, Fx2, Fx3, Fx4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            PreparareForm(0, 14, -100, 220);
+            Fx3 = FormatSeries("v1(t)", Color.Green);
+            Fx4 = FormatSeries("v2(t)", Color.LightGreen);
+            Fx1 = FormatSeries("h1(t)", Color.Blue);
+            Fx2 = FormatSeries("h2(t)", Color.LightBlue);
+
+            double h1 = DrawEuler(DELTA_T, Fx3, Fx1);
+            double h2 = DrawEuler(DELTA_T / 2, Fx4, Fx2);
+            double a = Math.Abs(h2 - h1);
+            double b = a / h1;
+            richTextBox1.AppendText("|h1 - h2| = " + FormatNumber(a) + "\n");
+            richTextBox1.AppendText("|h1 - h2| / h1 = " + FormatNumber(b) + "\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            PreparareForm(0, 14, -100, 220);
+            Fx3 = FormatSeries("v1(t)", Color.Green);
+            Fx4 = FormatSeries("v2(t)", Color.LightGreen);
+            Fx1 = FormatSeries("h1(t)", Color.Blue);
+            Fx2 = FormatSeries("h2(t)", Color.LightBlue);
+
+            double h1 = DrawRk(DELTA_T, Fx3, Fx1);
+            double h2 = DrawRk(DELTA_T / 2, Fx4, Fx2);
+            double a = Math.Abs(h2 - h1);
+            double b = a / h1;
+            richTextBox1.AppendText("|h1 - h2| = " + FormatNumber(a) + "\n");
+            richTextBox1.AppendText("|h1 - h2| / h1 = " + FormatNumber(b) + "\n");
+        }
+
+        private double DrawEuler(double deltaT, Series Fx1, Series Fx2)
+        {
+            double v = V0;
+            double v0 = V0;
+            double h = H0;
+            double maxH = 0;
+            double maxHT = 0;
+            double groundT = 0;
+
+            for (double t = 0; ; t += deltaT)
+            {
+                Fx1.Points.AddXY(t, v);
+                Fx2.Points.AddXY(t, h);
+                if (v0 >= 0 && v <= 0)
+                {
+                    maxH = h;
+                    maxHT = t;
+                }
+                if (h <= 0)
+                {
+                    groundT = t;
+                    break;
+                }
+                v0 = v;
+                var d = deltaT * A(v, t);
+                h += v * deltaT;
+                v += d;
+            }
+
+            richTextBox1.AppendText("\nEulerio, kai žingsnis t = " + deltaT + ":\n");
+            richTextBox1.AppendText("Maksimalus aukštis = " + FormatNumber(maxH) + "m, kai t = " + FormatNumber(maxHT) + "s\n");
+            richTextBox1.AppendText("Aukštis = 0m, kai t = " + FormatNumber(groundT) + "s\n");
+
+            return maxH;
+        }
+
+        private double DrawRk(double deltaT, Series Fx1, Series Fx2)
+        {
+            double v = V0;
+            double h = H0;
+            double v0 = V0;
+            double maxH = 0;
+            double maxHT = 0;
+            double groundT = 0;
+
+            for (double t = 0; ; t += deltaT)
+            {
+                Fx1.Points.AddXY(t, v);
+                Fx2.Points.AddXY(t, h);
+                if (v0 >= 0 && v <= 0)
+                {
+                    maxH = h;
+                    maxHT = t;
+                }
+                if (h <= 0)
+                {
+                    groundT = t;
+                    break;
+                }
+                v0 = v;
+
+                var deltaT2 = deltaT / 2;
+                var v1 = v + deltaT2 * A(v, t);
+                var v2 = v + deltaT2 * A(v1, t + deltaT2);
+                var v3 = v + deltaT * A(v2, t + deltaT2);
+                h += v * deltaT;
+                v += deltaT / 6 * (A(v, t) + 2 * A(v1, t + deltaT2) + 2 * A(v2, t + deltaT2) + A(v3, t + deltaT));
+
+            }
+
+            richTextBox1.AppendText("\nIV Eilės RK, kai žingsnis = " + deltaT + "\n:");
+            richTextBox1.AppendText("Maksimalus aukštis = " + FormatNumber(maxH) + "m, kai t = " + FormatNumber(maxHT) + "s\n");
+            richTextBox1.AppendText("Aukštis = 0m, kai t = " + FormatNumber(groundT) + "s\n");
+
+            return maxH;
+        }
+
+        private double A(double v, double t)
+        {
+            if (v > 0)
+            {
+                return -(G + K1 * v * v / M);
+            }
+            return -(G - K2 * v * v / M);
+        }
+
+        private double Y(double y, double x)
+        {
+            return -y + 1;
+        }
+
+        private string FormatNumber(double t)
+        {
+            return string.Format("{0:0.000000}", t);
+        }
+
+        private Series FormatSeries(string title, Color color)
+        {
+            Series f;
+            f = chart1.Series.Add(title);
+            f.ChartType = SeriesChartType.Line;
+            f.BorderWidth = 3;
+            f.Color = color;
+
+            return f;
+        }
+    }
+}
